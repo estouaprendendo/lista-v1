3231ee136ee72bbb500db729df9b6ca82b8e0f3c
Estou Aprendendo <estouaprendendo@gmail.com>
2016-11-24 02:46:09 -0200

    Meu primeiro commit

---
+++ src/main/java/com/lista/v1/ListaV1Application.java
@@ -0,0 +1,12 @@
+package com.lista.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaV1Application.class, args);
+	}
+}
